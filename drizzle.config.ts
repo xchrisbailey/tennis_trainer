c2b562754be54359c5b2a517aedc9e620ade90fe
Chris Bailey <chris@ghostly.io>
2024-07-21 03:05:20 -0400

    ✨ feat: lucia base setup w/ drizzle kit

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "drizzle-kit";
+import * as dotenv from "dotenv";
+dotenv.config({
+  path: [".env.local", ".env"],
+});
+
+export default defineConfig({
+  dialect: "sqlite",
+  schema: "./src/lib/db/schema.ts",
+  out: "./drizzle",
+  dbCredentials: {
+    url: process.env.DB_URL!,
+  },
+});
---
+++ src/lib/db/schema.ts
@@ -0,0 +1,22 @@
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { generateId } from "lucia";
+
+export const users = sqliteTable("user", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => generateId(15)),
+  email: text("email").unique(),
+  password: text("password"),
+});
+
+export type User = typeof users.$inferSelect;
+
+export const sessions = sqliteTable("session", {
+  id: text("id", { length: 255 }).primaryKey(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => users.id),
+  expiresAt: integer("expires_at").notNull(),
+});
+
+export type Session = typeof sessions.$inferSelect;
