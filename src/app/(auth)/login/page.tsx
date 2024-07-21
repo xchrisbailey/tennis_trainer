8c594f75f71f9b0097441d293957b974923ec5a4
Chris Bailey <chris@ghostly.io>
2024-07-21 05:03:27 -0400

    ♻️refactor: auth route grouping and single actions files

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,27 @@
+import { validateRequest } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { login_action } from "../_actions";
+
+export default async function Page() {
+  const { user } = await validateRequest();
+  if (user) {
+    return redirect("/");
+  }
+
+  return (
+    <>
+      <h1>Login</h1>
+      <form action={login_action}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" />
+        </div>
+        <button>Login</button>
+      </form>
+    </>
+  );
+}
---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,28 @@
+import { validateRequest } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { register_action } from "../_actions";
+
+export default async function Page() {
+  const { user } = await validateRequest();
+  if (user) {
+    return redirect("/");
+  }
+
+  return (
+    <>
+      <h1>Register</h1>
+      <form action={register_action}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" placeholder="email address" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" />
+        </div>
+
+        <button>Register</button>
+      </form>
+    </>
+  );
+}
